a441b7814c595d82c73272bacb052eae2453e6b2
joe <orbitalotter1@gmail.com>
2022-06-15 13:33:37 +1200

    Fixed some weighting issues and added the topic entry box plus enter button

--- overlay layout.py
+++ overlay layout.py
@@ -6,9 +6,11 @@ window.title("overlay")
 window.geometry("500x700")
 window.configure(background="white")
 
-window.rowconfigure(0, weight=2)
-window.rowconfigure(1, weight=1)
-window.rowconfigure(2, weight=8)
+font=("Noto Sans SemiBold", 14)
+
+window.rowconfigure(0, weight=1)
+window.rowconfigure(1, weight=4)
+window.rowconfigure(2, weight=1)
 
 window.columnconfigure(0, weight=1)
 
@@ -19,6 +21,7 @@ def format_image(i, size):
     image = ImageTk.PhotoImage(image)
     return image
 
+
 title = Frame(window, bg="Green")
 instructions = Frame(window, bg="Blue")
 topic_frame = Frame(window, bg="Orange")
@@ -27,14 +30,21 @@ title.grid(row=0, column=0, sticky="nsew")
 instructions.grid(row=1, column=0, sticky="nsew")
 topic_frame.grid(row=2, column=0, sticky="nsew")
 
-blank=PhotoImage()
-w,h=500,419
+blank = PhotoImage()
+w, h = 500, 419
 instructions.rowconfigure(0, weight=1)
 instructions.columnconfigure(0, weight=1)
-instructions_image= format_image("cos_instructions.png", (w,h))
-instructions_label=Label(instructions, image=instructions_image, bg="#0D0D13")
+instructions_image = format_image("cos_instructions.png", (w, h))
+instructions_label = Label(instructions, image=instructions_image, bg="#0D0D13")
 instructions_label.grid(row=0, column=0, sticky="nsew")
 
+topic_frame.columnconfigure(0, weight=5)
+topic_frame.columnconfigure(1,weight=1)
+topic_frame.rowconfigure(0,weight=1)
+topic=Entry(topic_frame, font=font)
+enter=Button(topic_frame, text="Enter", font=font)
 
+topic.grid(row=0, column=0, sticky="nsew", pady=20,padx=10)
+enter.grid(row=0, column=1, sticky="nsew", pady=20, padx=50)
 
 mainloop()
