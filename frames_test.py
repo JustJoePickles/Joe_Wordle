54641e70c43e3da4bc378a3c3cbd590e3fcb2f55
joe <orbitalotter1@gmail.com>
2022-06-16 12:37:59 +1200

    Created the completed keyboard, changed the info image to match the colour scheme. Made other significant syntax changes.

--- frames_test.py
+++ frames_test.py
@@ -19,6 +19,7 @@ class MainProgram():
 
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=1)
+        # self.change_frame("homepage")
         self.root.mainloop()
 
     def change_frame(self, page_name):
@@ -46,8 +47,6 @@ class Overlay():
 
         self.window.columnconfigure(0, weight=1)
 
-
-
         title = Frame(self.window, bg="orange")
         instructions = Frame(self.window, bg="Blue")
         topic_frame = Frame(self.window, bg="#0D0D13")
@@ -69,10 +68,11 @@ class Overlay():
         topic = Entry(topic_frame, font=self.font)
         enter = Button(topic_frame, text="Enter", font=self.font, command=lambda: root.change_frame("homepage"))
 
-        topic.grid(row=0, column=0, sticky="nsew", pady=20, padx=10)
-        enter.grid(row=0, column=1, sticky="nsew", pady=20, padx=50)
+        topic.grid(row=0, column=0, sticky="nsew", pady=(0, 20), padx=10)
+        enter.grid(row=0, column=1, sticky="nsew", pady=(0, 20), padx=(20, 30))
         instructions.update()
         print(instructions.winfo_width(), instructions.winfo_height())
+
 
 class HomePage():
     def __init__(self, root):
@@ -95,7 +95,7 @@ class HomePage():
         self.timer = Frame(self.window, bg="green")
         self.topic = Frame(self.window, bg="yellow")
         self.letters = Frame(self.window, bg="pink")
-        self.keyboard = Frame(self.window, bg="orange")
+        self.keyboard = Frame(self.window, bg="#0D0D13")
 
         self.information.grid(row=0, column=0, sticky="nsew")
         self.title.grid(row=0, column=1, sticky="nsew")
@@ -103,18 +103,36 @@ class HomePage():
         self.topic.grid(row=1, column=1, sticky="nsew")
         self.letters.grid(row=2, column=0, columnspan=3, sticky="nsew")
         self.keyboard.grid(row=3, column=0, columnspan=3, sticky="nsew")
-        self.info_image = root.format_image("info.png", (50, 50))
 
+        self.keyboard_pad = Frame(self.keyboard, bg="blue")
+        self.keyboard_pad.pack(side=TOP, fill="both", expand=True, padx=20, pady=10)
+        self.row_zero = Frame(self.keyboard_pad, bg="#0D0D13")
+        self.row_one = Frame(self.keyboard_pad, bg="#0D0D13")
+        self.row_two = Frame(self.keyboard_pad, bg="#0D0D13")
+
+        self.keyboard_pad.columnconfigure(0, weight=1)
+        for i in range(3):
+            self.keyboard_pad.rowconfigure(i, weight=1)
+        spacer = Label(self.row_one, width=20, height=20, image=self.blank, compound='c', bg="#0D0D13")
+        spacer.pack(side="left", fill="both", expand=False, padx=1, pady=1)
+
+        self.row_zero.grid(row=0, column=0, sticky="nsew")
+        self.row_one.grid(row=1, column=0, sticky="nsew")
+        self.row_two.grid(row=2, column=0, sticky="nsew")
+
+        # self.title_image = root.format_image
+
+        self.info_image = root.format_image("info.png", (50, 50))
         self.infobutton = Button(self.information, image=self.info_image, width=10, height=10, compound="c",
                                  relief="flat",
                                  borderwidth=0,
-                                 bg="white", activebackground="white", command=lambda: root.change_frame("overlay"))
+                                 bg="#0D0D13", activebackground="#0D0D13", command=lambda: root.change_frame("overlay"))
         self.infobutton.pack(side=LEFT, expand=True, fill='both')
         self.grid()
+        self.keyboard_maker()
 
     def grid(self):
-        x = 6
-        z = 5
+        x, z = 6, 5
         for i in range(x * z):
             b = Label(self.letters, width=20, height=20, image=self.blank, font=("Noto Sans SemiBold", 45),
                       text=str(i)[0],
@@ -124,6 +142,39 @@ class HomePage():
             self.letters.columnconfigure(i, weight=1)
         for i in range(x + (z - x)):
             self.letters.rowconfigure(i, weight=1)
+
+    def keyboard_maker(self):
+        def label_maker(type, i):
+            b = Button(type, width=20, height=20, image=self.blank, font=("Noto Sans SemiBold", 20),
+                       text=letters[i],
+                       compound='c', bg="#29292E", fg="white", relief="flat")
+            b.pack(side="left", fill="both", expand=True, padx=1, pady=1)
+
+        letters = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "A", "S", "D", "F", "G", "H", "J", "K", "L", "",
+                   "⌫", "Z", "X", "C", "V", "B", "N", "M", "Enter", ""]
+
+        x, z = 10, 3
+        for i in range(x * z):
+
+            if math.floor(i / x) == 0:
+                label_maker(self.row_zero, i)
+
+            if math.floor(i / x) == 1:
+                if i % x != 9:
+                    label_maker(self.row_one, i)
+
+            if math.floor(i / x) == 2:
+                if i % x == 0 or i % x==8:
+                    b = Button(self.row_two, width=50, height=20, image=self.blank, font=("Noto Sans SemiBold", 15),
+                               text=letters[i],
+                               compound='c', bg="#29292E", fg="white", relief="flat")
+                    b.pack(side="left", fill="both", expand=True, padx=1, pady=1)
+                elif i % x == 9:
+                    pass
+                else:
+                    label_maker(self.row_two, i)
+        spacer = Label(self.row_one, width=20, height=20, image=self.blank, compound='c', bg="#0D0D13")
+        spacer.pack(side="left", fill="both", expand=False, padx=1, pady=1)
 
 
 if __name__ == "__main__":
