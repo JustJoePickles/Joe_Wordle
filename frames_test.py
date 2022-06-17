69859b7c1bd3d9b7e5ee1f8b09583577f01ec09a
joe <orbitalotter1@gmail.com>
2022-06-17 18:25:38 +1200

    created the label for the topic, added the title into the overlay menu, changed all colours to final colours and removed the redundant label_pad

--- frames_test.py
+++ frames_test.py
@@ -9,7 +9,7 @@ class MainProgram():
         self.root.title("Joedle")
         self.root.geometry("500x700")
         self.root.configure(background="white")
-        self.images=["two_outof_two.png", "one_outof_two.png", "zero_outof_two.png"]
+        self.images = ["two_outof_two.png", "one_outof_two.png", "zero_outof_two.png"]
         self.homepage = HomePage(self)
         self.overlay = Overlay(self)
         self.frames = {}
@@ -20,7 +20,7 @@ class MainProgram():
 
         self.root.rowconfigure(0, weight=1)
         self.root.columnconfigure(0, weight=1)
-        self.change_frame("homepage")
+        # self.change_frame("homepage")
         self.root.mainloop()
 
     def change_frame(self, page_name):
@@ -66,20 +66,24 @@ class Overlay():
         topic_frame.columnconfigure(0, weight=5)
         topic_frame.columnconfigure(1, weight=1)
         topic_frame.rowconfigure(0, weight=1)
+
+        self.title_image = root.format_image("joedle.png", (100, 25))
+        title_screen = Label(title, image=self.title_image, width=10, height=20, compound="c", bg="#0D0D13")
+        title_screen.pack(side=LEFT, expand=True, fill='both')
+
         topic = Entry(topic_frame, font=self.font)
         enter = Button(topic_frame, text="Enter", font=self.font, command=lambda: root.change_frame("homepage"))
 
         topic.grid(row=0, column=0, sticky="nsew", pady=(0, 20), padx=10)
         enter.grid(row=0, column=1, sticky="nsew", pady=(0, 20), padx=(20, 30))
         instructions.update()
-        print(instructions.winfo_width(), instructions.winfo_height())
 
 
 class HomePage():
     def __init__(self, root):
         self.reference = "homepage"
         self.blank = PhotoImage()
-        self.window = Frame(root.root, bg="red", width=0, height=0)
+        self.window = Frame(root.root, bg="#0D0D13", width=0, height=0)
         self.window.grid(row=0, column=0, sticky="nsew")
         # self.window.pack(side=LEFT, expand=True, fill='both')
         self.window.columnconfigure(0, weight=1)
@@ -95,25 +99,23 @@ class HomePage():
         self.title = Frame(self.window, bg="red")
         self.timer = Frame(self.window, bg="green")
         self.topic = Frame(self.window, bg="yellow")
-        self.letters = Frame(self.window, bg="pink")
+        self.letters = Frame(self.window, bg="#0D0D13")
         self.keyboard = Frame(self.window, bg="#0D0D13")
 
         self.information.grid(row=0, column=0, sticky="nsew")
         self.title.grid(row=0, column=1, sticky="nsew")
         self.timer.grid(row=0, column=2, sticky="nsew")
         self.topic.grid(row=1, column=1, sticky="nsew")
-        self.letters.grid(row=2, column=0, columnspan=3, sticky="nsew")
-        self.keyboard.grid(row=3, column=0, columnspan=3, sticky="nsew")
+        self.letters.grid(row=2, column=0, columnspan=3, sticky="nsew",padx=10)
+        self.keyboard.grid(row=3, column=0, columnspan=3, sticky="nsew",padx=20,pady=10)
 
-        self.keyboard_pad = Frame(self.keyboard, bg="blue")
-        self.keyboard_pad.pack(side=TOP, fill="both", expand=True, padx=20, pady=10)
-        self.row_zero = Frame(self.keyboard_pad, bg="#0D0D13")
-        self.row_one = Frame(self.keyboard_pad, bg="#0D0D13")
-        self.row_two = Frame(self.keyboard_pad, bg="#0D0D13")
+        self.row_zero = Frame(self.keyboard, bg="#0D0D13")
+        self.row_one = Frame(self.keyboard, bg="#0D0D13")
+        self.row_two = Frame(self.keyboard, bg="#0D0D13")
 
-        self.keyboard_pad.columnconfigure(0, weight=1)
+        self.keyboard.columnconfigure(0, weight=1)
         for i in range(3):
-            self.keyboard_pad.rowconfigure(i, weight=1)
+            self.keyboard.rowconfigure(i, weight=1)
         spacer = Label(self.row_one, width=20, height=20, image=self.blank, compound='c', bg="#0D0D13")
         spacer.pack(side="left", fill="both", expand=False, padx=1, pady=1)
 
@@ -121,22 +123,25 @@ class HomePage():
         self.row_one.grid(row=1, column=0, sticky="nsew")
         self.row_two.grid(row=2, column=0, sticky="nsew")
 
-        self.timer_image = root.format_image(root.images[2], (50,50))
+        self.timer_image = root.format_image(root.images[2], (50, 50))
         self.timer_screen = Label(self.timer, image=self.timer_image, width=5, height=5, bg="#0D0D13")
         self.timer_screen.pack(side=LEFT, expand=True, fill='both')
 
-        self.title_image = root.format_image("joedle.png",(400,90))
-
-
-
-        self.title_screen = Label(self.title, image=self.title_image, width=10,height=20, compound="c", bg="#0D0D13")
+        self.title_image = root.format_image("joedle.png", (400, 90))
+        self.title_screen = Label(self.title, image=self.title_image, width=10, height=20, compound="c", bg="#0D0D13")
         self.title_screen.pack(side=LEFT, expand=True, fill='both')
+
         self.info_image = root.format_image("info.png", (50, 50))
         self.infobutton = Button(self.information, image=self.info_image, width=10, height=10, compound="c",
                                  relief="flat",
                                  borderwidth=0,
                                  bg="#0D0D13", activebackground="#0D0D13", command=lambda: root.change_frame("overlay"))
         self.infobutton.pack(side=LEFT, expand=True, fill='both')
+
+        self.topic_label = Label(self.topic, bg="#0D0D13", fg="white", text="example label",
+                                 font=("Noto Sans SemiBold", 20), image=self.blank, compound="c", width=10, height=10)
+        self.topic_label.pack(side=LEFT, expand=True, fill='both')
+
         self.grid()
         self.keyboard_maker()
 
@@ -144,9 +149,9 @@ class HomePage():
         x, z = 6, 5
         for i in range(x * z):
             b = Label(self.letters, width=20, height=20, image=self.blank, font=("Noto Sans SemiBold", 45),
-                      text=str(i)[0],
-                      compound='c')
-            b.grid(row=math.floor(i / x), column=i % x, sticky="nsew", padx=1, pady=1)
+                      text="",
+                      compound='c', bg="#29292E", fg="white")
+            b.grid(row=math.floor(i / x), column=i % x, sticky="nsew", padx=2, pady=1)
         for i in range(x):
             self.letters.columnconfigure(i, weight=1)
         for i in range(x + (z - x)):
@@ -173,7 +178,7 @@ class HomePage():
                     label_maker(self.row_one, i)
 
             if math.floor(i / x) == 2:
-                if i % x == 0 or i % x==8:
+                if i % x == 0 or i % x == 8:
                     b = Button(self.row_two, width=50, height=20, image=self.blank, font=("Noto Sans SemiBold", 15),
                                text=letters[i],
                                compound='c', bg="#29292E", fg="white", relief="flat")
